4e2ea190fd164ec5d60606e8d08ca44bf47aac6b
Azolile Mbanga <u24820360@tuks.co.za>
2026-08-12 22:30:00 +0200

    fix infinite loop

--- FilterLock.java
+++ FilterLock.java
@@ -10,15 +10,16 @@ public class FilterLock implements Lock
         this.n = n;                                     //the number of competing threads
         this.level = new VolatileInt[n];                //the levels where the other threads wait in before progressing closer to the critical section
         this.victim = new VolatileInt[n];               //one thread is the victim at each level and other threads can pass through if there is a victim
-        for (int i = 0; i < n; i++) {                   //set levels to 0.
+        for (int i = 0; i < n; i++) {                   //set levels and victim to 0.
             this.level[i] = new VolatileInt(0);
+            this.victim[i] = new VolatileInt(0);
         }
     }
 
     @Override
     public void lock(int threadId) 
     {
-        for (int L = 0; L < this.n - 1; L++) {          //n threads require n - 1 levels because one thread must enter the critical section.
+        for (int L = 1; L < this.n - 1; L++) {          //n threads require n - 1 levels because one thread must enter the critical section.
             this.level[threadId].value = L;             //thread with ID, threadId enters level L
             this.victim[L].value = threadId;            //thread with ID, threadId announces itself as the victim
             while (ahead(threadId) && this.victim[L].value == threadId) {
